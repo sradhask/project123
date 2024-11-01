f7d5a970d7236d210e35a1ad2b9215e38595eecc
sradhask <sradhask345@gmail.com>
2024-11-01 08:47:45 +0530

    done

--- t4/webpro/webapp/views.py
+++ t4/webpro/webapp/views.py
@@ -30,21 +30,26 @@ def logout_view(request):
         return redirect('login')
 
 def register(request):
-        if request.method =='POST':
-            name=request.POST['name']
-            email=request.POST['email']
-            password=request.POST['password']
-            ConfirmPassword=request.POST['ConfirmPassword']
-            try:
-                user=User.objects.create_user(username=email,email=email,password=password,ConfirmPassword=ConfirmPassword)
-                user.save()
-            except:
-                messages.error(request, "Email exit.")
+    if request.method == 'POST':
+        fullname = request.POST['fullname']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['ConfirmPassword']
 
-                return redirect('register')
-            return redirect('login')
-        else:
-            return render(request,'register.html')
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match.")
+            return redirect('register')
+
+        try:
+            user = User.objects.create_user(username=fullname, email=email, password=password)
+            user.save()
+            messages.success(request, "Registration successful. You can now log in.")
+            return redirect("user_login")
+        except:
+            messages.error(request, "Email already exists.")
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
 def userhome(request):
     if 'username' in request.session:
      data=Product.objects.all()
@@ -62,6 +67,9 @@ def seller(request):
      if request.method =='POST':
             email=request.POST['email']
             password=request.POST['password']
+            
+             confirm_password = request.POST['ConfirmPassword']
+            
           
             try:
                 user=User.objects.create_user(username=email,email=email,password=password)
@@ -133,7 +141,6 @@ def add_product(request):
       return render("seller2.html")
     else:
         return redirect("seller1.html")
-      
       
 def seller1(request):
     return render(request,"seller1.html")
